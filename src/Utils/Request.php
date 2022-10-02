06943146b5f54af61615e68e67c2f3b4a478a21b
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2022-10-02 17:18:19 +0700

    add: request variable handler

---
+++ src/Utils/Request.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Selamatan\App\Utils;
+
+class Request
+{
+    private $_data;
+
+    public function __construct()
+    {
+        $this->_data = $_REQUEST;
+    }
+
+    public function __set($name, $value)
+    {
+        return $this->_data[$name] = $value;
+    }
+
+    public function __get($name)
+    {
+        return array_key_exists($name, $this->_data)
+            ? $this->_data[$name] : null;
+    }
+}
